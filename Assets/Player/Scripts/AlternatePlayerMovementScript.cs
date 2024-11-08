b7c19f58d6bd652a8a07d3bf4ec62a21fcd8e478
sanjay-b-08 <sanjaybadri08@gmail.com>
2024-11-08 12:14:46 -0500

    Updated Player Movement Script + Camera Follow
    Placeholder map provided with colors

---
+++ Assets/Player/Scripts/AlternatePlayerMovementScript.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class AlternatePlayerMovementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Boolean moving;
+    private Vector2 input;
+    public float moveSpeed = 5f;
+
+    public Animator anim;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!moving)
+        {
+            input.x = UnityEngine.Input.GetAxisRaw("Horizontal");
+            input.y = UnityEngine.Input.GetAxisRaw("Vertical");
+
+            if (input != Vector2.zero)
+            {
+                var tarPos = transform.position;
+                tarPos.x += input.x;
+                tarPos.y += input.y;
+
+                StartCoroutine(Move(tarPos));
+            }
+        }
+
+        if (input.x != 0)
+        {
+            input.y = 0;
+        }
+
+        anim.SetFloat("moveX", input.x);
+        anim.SetFloat("moveY", input.y);
+    }
+
+    IEnumerator Move(Vector3 tarPos)
+    {
+        moving = true;
+
+        while ((tarPos - transform.position).sqrMagnitude > Mathf.Epsilon) {
+            transform.position = Vector3.MoveTowards(transform.position, tarPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = tarPos;
+
+        moving = false;
+    }
+}
