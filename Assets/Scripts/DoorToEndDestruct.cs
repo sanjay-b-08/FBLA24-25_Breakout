a6af58d3421dc1c6e8a969d8dba8ebd38acd5895
sanjay-b-08 <sanjaybadri08@gmail.com>
2024-11-16 15:12:38 -0500

    end screen

--- Assets/Scripts/DoorToEndDestruct.cs
+++ Assets/Scripts/DoorToEndDestruct.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class DoorToEndDestruct : MonoBehaviour
 {
     // Start is called before the first frame update
     public Tilemap doorToEnd;
+
+    public Image congratsScreen;
+    public TextMeshProUGUI congratsText;
+    public Button playAgainButton;
+    public TextMeshProUGUI playAgainText;
 
     public void endDestruct()
     {
@@ -14,5 +21,38 @@ public class DoorToEndDestruct : MonoBehaviour
         doorToEnd.GetComponent<TilemapCollider2D>().enabled = false;
         doorToEnd.GetComponent<CompositeCollider2D>().enabled = false;
         doorToEnd.GetComponent<BoxCollider2D>().enabled = false;
+
+        void GameOver()
+        {
+            if (congratsScreen.color.a < 1)
+            {
+                var colorAlpha = congratsScreen.color;
+                colorAlpha.a += 0.01f;
+                congratsScreen.color = colorAlpha;
+            }
+
+            if (congratsText.color.a < 1)
+            {
+                var colorAlpha = congratsText.color;
+                colorAlpha.a += 0.005f;
+                congratsText.color = colorAlpha;
+            }
+
+            playAgainButton.GetComponent<Button>().enabled = true;
+
+            if (playAgainButton.GetComponent<Image>().color.a < 1)
+            {
+                var colorAlpha = playAgainButton.GetComponent<Image>().color;
+                colorAlpha.a += 0.005f;
+                playAgainButton.GetComponent<Image>().color = colorAlpha;
+            }
+
+            if (playAgainText.color.a < 1)
+            {
+                var colorAlpha = playAgainText.color;
+                colorAlpha.a += 0.005f;
+                playAgainText.color = colorAlpha;
+            }
+        }
     }
 }
